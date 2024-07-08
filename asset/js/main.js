de1232f47060366e2fc8a5ed94db5b281df0ac03
Lainieeeee <yorin0820@gmail.com>
2024-07-08 15:46:02 +0800

    update

--- asset/js/main.js
+++ asset/js/main.js
@@ -21,7 +21,7 @@ $(function () {
 $(function(){
   $('a[href^="#"]').click(function(){
     var adjust = 0;
-    var speed = 100;
+    var speed = 200;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top + adjust;
